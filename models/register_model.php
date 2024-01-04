ebcae314d3f71ac7ca4a5ff8fba53a0e28f82649
Opaleye Olabode Gabriel <consultingbrightside@gmail.com>
2024-01-04 14:59:26 +0100

    Edit security on email

--- models/register_model.php
+++ models/register_model.php
@@ -152,8 +152,8 @@ use PHPMailer\PHPMailer\Exception;
           $mail->isSMTP();                                            //Send using SMTP
           $mail->Host       = 'smtp.elasticemail.com';                     //Set the SMTP server to send through
           $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-          $mail->Username   = 'hr@dreamcityhes.com';                     //SMTP username
-          $mail->Password   = '1C3DF7FB5653C8E2A817D344119E5461A4F7';                               //SMTP password
+          $mail->Username   = DEMAIL;                     //SMTP username
+          $mail->Password   = DPWD;                               //SMTP password
          // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
           //$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
           // Recipients
